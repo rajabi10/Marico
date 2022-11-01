8b453cfc7982dd6e783d37975feb8869a4100654
Negin Rajabi <negin171378@gmail.com>
2022-11-01 23:47:30 +0330

    add navbar function for toggle

---
+++ js/main.js
@@ -0,0 +1,5 @@
+const toggler = document.querySelector(".nav__toggler");
+const navbar = document.querySelector(".nav");
+toggler.addEventListener("click", (e) => {
+  navbar.classList.toggle("nav__expanded");
+});
